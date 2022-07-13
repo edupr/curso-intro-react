401f6702dec7154bc71f27479ccb388aa5655934
Edu <particularspam@gmail.com>
2022-07-13 13:13:29 -0500

    CSS styles applied

--- src/App.js
+++ src/App.js
@@ -4,29 +4,32 @@ import { TodoSearch } from "./TodoSearch";
 import { TodoList } from "./TodoList";
 import { TodoItem } from "./TodoItem";
 import { CreateTodoButton } from "./CreateTodoButton";
-// import "./App.css";
+// import './App.css';
 
 const todos = [
-  { text: "Cortar cebolla", completed: false },
-  { text: "Tomar el curso de React", completed: false },
+  { text: "Cortar cebolla", completed: true },
+  { text: "Tomar el cursso de intro a React", completed: false },
   { text: "Llorar con la llorona", completed: false },
+  { text: "LALALALAA", completed: false },
 ];
 
 function App() {
   return (
     <React.Fragment>
       <TodoCounter />
-
       <TodoSearch />
 
-      <TodoList />
-      {todos.map((todo) => (
-        <TodoItem key={todo.text} text={todo.text} />
-      ))}
-      <TodoList />
+      <TodoList>
+        {todos.map((todo) => (
+          <TodoItem
+            key={todo.text}
+            text={todo.text}
+            completed={todo.completed}
+          />
+        ))}
+      </TodoList>
 
       <CreateTodoButton />
-      <button>+</button>
     </React.Fragment>
   );
 }
