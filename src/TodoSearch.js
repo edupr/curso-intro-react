79463e031b52e6fdbc0dc6989ca4d9ae990c41d6
Edu <particularspam@gmail.com>
2022-07-13 23:11:22 -0500

    Manejo de eventos

--- src/TodoSearch.js
+++ src/TodoSearch.js
@@ -2,7 +2,17 @@ import React from "react";
 import "./TodoSearch.css";
 
 function TodoSearch() {
-  return <input className="TodoSearch" placeholder="Buscar..." />;
+  const onSearchValueChange = (event) => {
+    console.log(event.target.value);
+  };
+
+  return (
+    <input
+      className="TodoSearch"
+      placeholder="Buscar..."
+      onChange={onSearchValueChange}
+    />
+  );
 }
 
 export { TodoSearch };
