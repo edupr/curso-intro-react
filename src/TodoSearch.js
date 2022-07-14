af7075822f4bb0db0f7794cd4030f21b6a225e76
Edu <particularspam@gmail.com>
2022-07-14 14:27:20 -0500

    Manejo del estado

--- src/TodoSearch.js
+++ src/TodoSearch.js
@@ -2,17 +2,22 @@ import React from "react";
 import "./TodoSearch.css";
 
 function TodoSearch() {
+  const [searchValue, setsearchValue] = React.useState("");
+
   const onSearchValueChange = (event) => {
     console.log(event.target.value);
+    setsearchValue(event.target.value);
   };
 
-  return (
+  return [
     <input
       className="TodoSearch"
       placeholder="Buscar..."
+      value={searchValue}
       onChange={onSearchValueChange}
-    />
-  );
+    />,
+    <p>{searchValue}</p>,
+  ];
 }
 
 export { TodoSearch };
