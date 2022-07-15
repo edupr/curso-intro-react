959a53da630c0aa55932ab033d6778f14ebe9a1d
Edu <particularspam@gmail.com>
2022-07-15 12:56:46 -0500

    Completing and deleting TODOs

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import React from 'react';
-import { TodoCounter } from './TodoCounter';
-import { TodoSearch } from './TodoSearch';
-import { TodoList } from './TodoList';
-import { TodoItem } from './TodoItem';
-import { CreateTodoButton } from './CreateTodoButton';
+import React from "react";
+import { TodoCounter } from "./TodoCounter";
+import { TodoSearch } from "./TodoSearch";
+import { TodoList } from "./TodoList";
+import { TodoItem } from "./TodoItem";
+import { CreateTodoButton } from "./CreateTodoButton";
 // import './App.css';
 
 const defaultTodos = [
-  { text: 'Cortar cebolla', completed: true },
-  { text: 'Tomar el curso de intro a React', completed: false },
-  { text: 'Llorar con la llorona', completed: true },
-  { text: 'LALALALAA', completed: true },
+  { text: "Cortar cebolla", completed: true },
+  { text: "Tomar el curso de intro a React!", completed: false },
+  { text: "Llorar con la llorona!", completed: false },
+  { text: "LALALALAA", completed: true },
 ];
 
 function App() {
   const [todos, setTodos] = React.useState(defaultTodos);
-  const [searchValue, setSearchValue] = React.useState('');
+  const [searchValue, setSearchValue] = React.useState("");
 
-  const completedTodos = todos.filter(todo => !!todo.completed).length;
+  const completedTodos = todos.filter((todo) => !!todo.completed).length;
   const totalTodos = todos.length;
 
   let searchedTodos = [];
@@ -25,30 +25,46 @@ function App() {
   if (!searchValue.length >= 1) {
     searchedTodos = todos;
   } else {
-    searchedTodos = todos.filter(todo => {
+    searchedTodos = todos.filter((todo) => {
       const todoText = todo.text.toLowerCase();
       const searchText = searchValue.toLowerCase();
       return todoText.includes(searchText);
     });
   }
-  
+
+  // Método completar TODOs
+  const completeTodo = (text) => {
+    const todoIndex = todos.findIndex((todo) => todo.text === text);
+    const newTodos = [...todos];
+    newTodos[todoIndex].completed = true;
+    setTodos(newTodos);
+
+    console.log("todoIndex: ", todoIndex);
+  };
+
+  // Método eliminar TODOs
+  const deleteTodo = (text) => {
+    const todoIndex = todos.findIndex((todo) => todo.text === text);
+    const newTodos = [...todos];
+    newTodos.splice(todoIndex, 1);
+    setTodos(newTodos);
+
+    console.log("todoIndex: ", todoIndex);
+  };
+
   return (
     <React.Fragment>
-      <TodoCounter
-        total={totalTodos}
-        completed={completedTodos}
-      />
-      <TodoSearch
-        searchValue={searchValue}
-        setSearchValue={setSearchValue}
-      />
+      <TodoCounter total={totalTodos} completed={completedTodos} />
+      <TodoSearch searchValue={searchValue} setSearchValue={setSearchValue} />
 
       <TodoList>
-        {searchedTodos.map(todo => (
+        {searchedTodos.map((todo) => (
           <TodoItem
             key={todo.text}
             text={todo.text}
             completed={todo.completed}
+            onComplete={() => completeTodo(todo.text)}
+            onDelete={() => deleteTodo(todo.text)}
           />
         ))}
       </TodoList>
--- src/TodoCounter.js
+++ src/TodoCounter.js
@@ -2,7 +2,7 @@ import React from "react";
 import "./TodoCounter.css";
 
 function TodoCounter({ total, completed }) {
-  return <h2 className="TodoCounter">Has completado {completed} de {total} TODOs</h2>;
+  return <h2 className="TodoCounter">Has completado {completed} de {total} TODOs!</h2>;
 }
 
 export { TodoCounter };
