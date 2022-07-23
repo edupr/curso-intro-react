3eb4ecf2335fb26682f90d34fec3d8f3bd144812
Edu <particularspam@gmail.com>
2022-07-22 20:44:45 -0500

    Updates

---
+++ precache-manifest.ee6929b23fef36ddb4564bece071c989.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "e5fde1fe3db6f2948747",
+    "url": "/curso-intro-react/static/css/main.f21c09f9.chunk.css"
+  },
+  {
+    "revision": "e5fde1fe3db6f2948747",
+    "url": "/curso-intro-react/static/js/main.e5fde1fe.chunk.js"
+  },
+  {
+    "revision": "ceefb4fbb137ac8ec744",
+    "url": "/curso-intro-react/static/js/1.ceefb4fb.chunk.js"
+  },
+  {
+    "revision": "8f31ac8995903ac72b62",
+    "url": "/curso-intro-react/static/js/runtime~main.8f31ac89.js"
+  },
+  {
+    "revision": "1a6f0b798ddd99c179aa051f63ba8bc6",
+    "url": "/curso-intro-react/static/media/check.1a6f0b79.svg"
+  },
+  {
+    "revision": "f6b8972e5a96e13f1e7e5164167ca7cd",
+    "url": "/curso-intro-react/static/media/delete.f6b8972e.svg"
+  },
+  {
+    "revision": "6aa74b4f406616be4b9468bc4252ed35",
+    "url": "/curso-intro-react/index.html"
+  }
+];
